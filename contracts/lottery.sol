aeb56fd203347bef4d39fd3cfc7972f39b9018ba
Justin Lo <kk123456235@gmail.com>
2022-02-13 02:32:32 +0800

    reset the address after picked up the winner

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -23,6 +23,7 @@ contract Lottery {
     function pickWinner() public payable {
         uint index = random() % players.length;
         players[index].transfer(address(this).balance);
+        players = new address payable[](0);
     }
 
 }
