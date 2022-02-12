e99c8c3a943f1c6f9cefe5d6643a6fb40b4bc244
Justin Lo <kk123456235@gmail.com>
2022-02-13 02:01:15 +0800

    Lottery basic contract

---
+++ contracts/lottery.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+    
+    constructor() {
+        manager = msg.sender;
+    }
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        
+        players.push(msg.sender);
+    }
+}   
