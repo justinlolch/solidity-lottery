7335ae5f2a302218ccdd9d85baff031f835c29ed
Justin Lo <kk123456235@gmail.com>
2022-02-13 02:10:43 +0800

    generate random number

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -5,13 +5,18 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Lottery {
     address public manager;
     address[] public players;
-    
+
     constructor() {
         manager = msg.sender;
     }
+
     function enter() public payable {
         require(msg.value > .01 ether);
-        
+
         players.push(msg.sender);
     }
-}   
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encode(block.difficulty, block.difficulty, players)));
+    }
+}
